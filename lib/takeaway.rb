150019aece4eb5d317b7afd0ba8c0edd1cfb26dd
Phil Smithies <philsmithies@gmail.com>
2021-04-14 09:48:42 +0200

    updated gitignore

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -32,24 +32,6 @@ class Takeaway
     time = Time.now + 3600 # seconds in an hour
     time.strftime("%k:%M")
   end
-
-  def sendtext
-   
-    # removed my sid/auth/mobile numbers for upload
-    account_sid = 'ACf213fd0e0968cad4cf1d53ef5e2fe301'
-    auth_token = 'f3a00b928bb594b9847b5253a4d5092c'
-    client = Twilio::REST::Client.new(account_sid, auth_token)
-
-    from = '+447723565836' # Your Twilio number
-    to = '+447709018333' # Your mobile phone number
-
-    client.messages.create(
-    from: from,
-    to: to,
-    body: "Thank you! The total was £#{total} Your order was placed 
-    and will be delivered before #{time}"
-    )
-  end
   
   def checkout  
     sendtext
